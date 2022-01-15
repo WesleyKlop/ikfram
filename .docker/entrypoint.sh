23c728fca7ef26fd8d236260982a9b4d809bb1c2
Wesley Klop <wesley19097@gmail.com>
2022-01-15 14:05:29 +0100

    Make sure storage directories exist

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -6,11 +6,17 @@ if [ -f .env ]; then
 fi
 
 if [ -z "$APP_KEY" ]; then
-    echo "No application key found, setting and then exiting. Please start again.";
+    echo "No application key found, setting and then exiting. Please start again."
     php artisan key:generate
-    exit 1;
+    exit 1
 fi
 
+mkdir -p \
+  storage/logs \
+  storage/framework/cache/data \
+  storage/framework/sessions \
+  storage/framework/views \
+  storage/app/public
 chown -R www-data:www-data /app/storage
 
 su www-data -s /bin/bash -c '
